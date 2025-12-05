d0ae27f244b4fdb022fa0254ba188a9a98121ce4
Long Trinh <long.trinh.rh@bp.renesas.com>
2025-12-05 11:37:22 +0700

    rz-cmn: rename RZV2H-SBC to IMDT-V2H-SBC
    Signed-off-by: Long Trinh <long.trinh.rh@bp.renesas.com>
    Signed-off-by: Tien Nguyen <tien.nguyen.uh@renesas.com>

--- include/configs/rz-cmn.h
+++ include/configs/rz-cmn.h
@@ -24,7 +24,7 @@
 #define BOARD_ID_RZV2L_EVK				0x20
 #define BOARD_ID_RZV2H_EVK				0x30
 #define BOARD_ID_RZV2H_RDK				0x31
-#define BOARD_ID_RZV2H_SBC				0x32
+#define BOARD_ID_IMDT_V2H_SBC			0x32
 
 /*
  * RZ Board SoC Identifiers
