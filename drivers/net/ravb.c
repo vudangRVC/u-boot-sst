cfd184e1256b6b20ba0b4a6cb718e76b1f8f469d
Phat Nguyen <phat.nguyen.xh@bp.renesas.com>
2026-01-19 09:12:41 +0000

    upstream: rz-cmn: Add missing dts and pinctrl configurations for RZV2L, RZG2L-EVK Update changes: - Add dts for RZV2L-EVK and RZG2L-EVK - Replace source and change pinctrl configurations Signed-off-by: Phat Nguyen <phat.nguyen.xh@bp.renesas.com>

--- drivers/net/ravb.c
+++ drivers/net/ravb.c
@@ -818,6 +818,8 @@ static const struct udevice_id ravb_ids[] = {
 	{ .compatible = "renesas,etheravb-rcar-gen3" },
 	{ .compatible = "renesas,etheravb-rcar-gen4" },
 	{ .compatible = "renesas,etheravb-r9a07g044l" },
+	{ .compatible = "renesas,etheravb-r9a07g054l" },
+	{ .compatible = "renesas,etheravb-r9a07g043f" },
 	{ }
 };
 
