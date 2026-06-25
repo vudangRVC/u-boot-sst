e87af61747e65b52b5d0c3738ef1e76b884f5e64
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2026-06-25 18:04:16 +0700

    board: renesas: rz-cmn: fix error sd card init on sparrowhawk.
    fix error sd card init on sparrowhawk.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- board/renesas/rz-cmn/spl_rz_cmn.c
+++ board/renesas/rz-cmn/spl_rz_cmn.c
@@ -227,12 +227,12 @@ void spl_perform_board_fixups(struct spl_image_info *spl_image)
 	if (soc_id != RZ_SOC_RCAR_V4H)
 		return;
 
-	// if (!renesas_v4h_sparrowhawk_is_evta1) {
-	// 	if (board_id == BOARD_ID_RCAR_V4H_SPARROWHAWK)
-	// 		printf("EVTB1 board detected (board_id=0x%lx)\n",
-	// 		       (unsigned long)board_id);
-	// 	return;
-	// }
+	if (!renesas_v4h_sparrowhawk_is_evta1) {
+		if (board_id == BOARD_ID_RCAR_V4H_SPARROWHAWK)
+			printf("EVTB1 board detected (board_id=0x%lx)\n",
+			       (unsigned long)board_id);
+		return;
+	}
 
 	if (!blob)
 		return;
